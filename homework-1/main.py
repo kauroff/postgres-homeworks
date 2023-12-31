769f657f350ff3b279b6fff2111b20468761acf1
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-12-31 15:11:20 +0300

    Edited first homework

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,31 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
-import psycopg2
+import psycopg2  # импортируем библиотеку psycopg2
 
 
-def function_of_reading(file_name):
+def function_of_reading(file_name):  # решил делать через функции чтения файла и записи в БД
     with open(file_name, encoding='utf-8') as file:
         return file.read()
 
 
 def function_of_appending(data, name_table):
-    try:
-        conn = psycopg2.connect(
+    try:  # оборачиваем в блок try
+        conn = psycopg2.connect(  # настраиваем соединение
             host="localhost",
             database="north",
             user="postgres",
             password="12345"
         )
-        conn.autocommit = True
+        conn.autocommit = True  # автокоммит, чтобы не коммитить после каждого добавления данных
         with conn.cursor() as cur:
-            list_of_data = data.split('\n')
-            if name_table == 'customers':
+            list_of_data = data.split('\n')  # беру данные из csv файла и сплитую их, чтобы разбить
+            if name_table == 'customers':  # делаю проверку на название таблицы, тк в разных таблицах разное кол-во столбцов
                 for element in list_of_data:
-                    if element.split(',') == 3:
-                        id, company, contact = element.split(',')
-                        cur.execute(f"INSERT INTO {name_table} VALUES (%s, %s, %s)", (id, company, contact))
+                    if element.split(',') == 3:  # проверка на последнюю пустую строку
+                        id, company, contact = element.split(',')  # распаковка строки на данные в кортеж
+                        cur.execute(f"INSERT INTO {name_table} VALUES (%s, %s, %s)",
+                                    (id, company, contact))  # добавление данных в таблицу
                     else:
                         continue
-            elif name_table == 'employees':
+            elif name_table == 'employees':  # такая же функция для каждой БД
                 for element in list_of_data:
                     if element.split(',') == 5:
                         id, name, surname, title, bday, notes = element.split(',')
@@ -41,16 +42,15 @@ def function_of_appending(data, name_table):
                                     (order, customer, employee, date, city))
                     else:
                         continue
-            cur.execute(f"SELECT * FROM {name_table}")
-            conn.commit()
+            cur.execute(f"SELECT * FROM {name_table}")  # вывод данных на экран
             rows = cur.fetchall()
             for row in rows:
                 print(row)
     finally:
-        conn.close()
+        conn.close()  # закрытие соединения
 
 
-#
+# работа с тремя БД
 customers = function_of_reading('north_data/customers_data.csv')
 employees = function_of_reading('north_data/employees_data.csv')
 orders = function_of_reading('north_data/orders_data.csv')
