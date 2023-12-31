23797f5ab643f3a1c1da8c44780572aea7ff5c2b
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-12-31 15:05:58 +0300

    Edited first homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -10,15 +10,15 @@ CREATE TABLE employees
 
 CREATE TABLE customers
 (
-	customer_id varchar(5) PRIMARY KEY,
+	customer_id varchar(10) PRIMARY KEY,
 	company_name varchar(100) NOT NULL,
 	contact_name varchar(100) NOT NULL
 );
 
 CREATE TABLE orders
 (
-	order_id serial NOT NULL,
-	customer_id varchar(5) REFERENCES customers(customer_id),
+	order_id serial PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id),
 	employee_id smallint NOT NULL REFERENCES employees(employee_id),
 	order_date date NOT NULL,
 	ship_city varchar(100) NOT NULL
