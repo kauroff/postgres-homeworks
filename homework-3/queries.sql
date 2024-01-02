5f77aa49e6f730f0bb7aca7d5425672f225e763f
Ivan Kaurov <crazyanimalcz@gamil.com>
2024-01-02 23:56:39 +0300

    Finished second homework

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,17 +1,21 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS fio_employee
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS fio_employee
 FROM customers
-JOIN employees USING(city)
-WHERE customers.city = 'London' AND employees.city = 'London' AND
+JOIN orders USING(customer_id)
+JOIN employees USING(employee_id)
 JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone
+FROM products
+JOIN suppliers USING(supplier_id)
+WHERE discounted = 0 AND units_in_stock < 25
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
